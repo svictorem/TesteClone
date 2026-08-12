5b49105b9177128746f1848b0d0943c69540d3da
Victor Emanuel <129910747+svictorem@users.noreply.github.com>
2026-08-12 13:39:18 -0400

    first commit

---
+++ src/main/java/br/com/cetam/testeclone/TesteCloneApplication.java
@@ -0,0 +1,13 @@
+package br.com.cetam.testeclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/cetam/testeclone/TesteCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cetam.testeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
